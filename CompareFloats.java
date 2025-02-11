c4889313c836ac61d24d91b8e5463cc2a9146c56
surekha maran <153985261+surekhamaran@users.noreply.github.com>
2025-02-11 22:35:59 +0530

    Update CompareFloats.java

--- CompareFloats.java
+++ CompareFloats.java
@@ -1,1 +1,12 @@
+class CompareFloats{
+  public static void main(String args[]){
+    Scanner sc= new Scanner();
+    System.out.println("enter the first float num1:");
+    float num1= Scanner.nextFloat();
+    System.out.println("enter the first float num2:");
+    float num2= Scanner.nextFloat();
+  }
+  public 
+}
 
+  
